08774e7f831d37c8ce119fde4dbd2f3b3a81c1d3
Ryan McCann <ryanjohnmccann@gmail.com>
2022-04-06 13:23:06 -0400

    Updated gitignore

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main(int argc, char *argv[]) {
 
     // Read command line input
-    char *input_fp = argv[2];
-    char *output_fp = argv[3];
+    char *in_file = argv[2];
+    char *out_file = argv[3];
     char *snap_interval = argv[4];
 
 
